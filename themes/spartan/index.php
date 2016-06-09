dd2cecc0650615b8f4b849b6700167b0e09d5705
Kirk Daniels <kdee15@gmail.com>
2016-06-09 09:39:37 +0200

    ok

--- themes/spartan/index.php
+++ themes/spartan/index.php
@@ -1,14 +1,2 @@
 <?php
-/**
- * The main template file
- *
- * @package WordPress
- * @subpackage Spartan
- * @since Spartan 1.0
- */
-
-get_header(); ?>
-
-<?php the_content(); ?>
-
-<?php get_footer(); ?>
+// Silence is golden.
