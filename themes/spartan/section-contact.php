59ba14081d233edd530238d46566d9e0cd31f3ec
Kirk Daniels <kdee15@gmail.com>
2016-06-09 09:16:12 +0200

    clean

--- themes/spartan/section-contact.php
+++ themes/spartan/section-contact.php
@@ -4,7 +4,34 @@
 
     <div class="container">
 
-        <h1>AWE</h1>
+        <h2>Contact Me</h2>
+
+        <!-- C.2.0.4. SOCIAL MEDIA -->
+
+        <ul class="social-media">
+        
+            <li><a href="http://www.facebook.com/kdee15">
+                <figure class="facebook icon">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/logo-facebook.svg" alt="fb">
+                </figure>
+                </a>
+            </li>
+        
+            <li><a href="http://www.twitter.com/kdee15">
+                <figure class="twitter icon">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/logo-twitter.svg" alt="fb">
+                </figure>
+                </a>
+            </li>
+        
+            <li><a href="https://za.linkedin.com/in/kirk-daniels-6049a61">
+                <figure class="linkedin icon">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/logo-youtube.svg" alt="fb">
+                </figure>
+                </a>
+            </li>
+
+        </ul>
 
     </div>                 
 
